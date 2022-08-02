c4b69f4e2fc89e45620ef5b93aac102543f017db
meherkalra <108827787+meherkalra@users.noreply.github.com>
2022-08-02 17:26:10 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,33 @@
+import json
+import os
+import requests
+import sys
+
+# BASE_URL = sys.argv[1]
+
+BASE_URL = "https://2d426e44-0b78-4936-a179-02556024fdbe.mock.pstmn.io"
+
+PATH1 = "/forms/elements"
+PATH2 = "/forms"
+DIR_PATH = "data/"
+
+X = "data"
+sub_file = [DIR_PATH + file for file in os.listdir(X)]
+
+for f in sub_file:
+    print(f)
+    for i in os.listdir(f + "/"):
+        path = os.path.join(f, i)
+        print(path)
+        with open(path, 'r') as f:
+            json_obj = json.load(f)
+            if "data/forms" in path:
+                r = requests.post(BASE_URL + PATH2, data=json_obj)
+            if "data/elements" in path:
+                r = requests.post(BASE_URL + PATH1, data=json_obj)
+            if r.status_code != 200:
+                print(r, "error")
+                print(path)
+            else:
+                print(r, "success")
+                print(path)
